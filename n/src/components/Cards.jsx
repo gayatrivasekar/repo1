96bd83a925028a2241ad72cafdcd4c056141c19c
gayatrivasekar <vasekargayatri3@gmail.com>
2025-07-11 14:58:05 +0530

    Initial commit

---
+++ n/src/components/Cards.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+const Cards = (props) => {
+  return (
+    <div className='containerr'>
+
+    <h1 id='name' >
+    {props.name}
+    </h1>
+
+
+    </div>
+  )
+}
+
+export default Cards
